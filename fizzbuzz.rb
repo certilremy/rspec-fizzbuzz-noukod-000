9eec67d2dce267d592c9c51fcae53e7c0e856cc2
Certil Remy <certil@noukod.com>
2018-08-31 16:30:46 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,16 +1,15 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
-elsif int % 5|| 3 == 0
+  
+if int % 5|| 3 == 0
   "FizzBuzz"
 
-  if int % 3 == 0
+elsif int % 3 == 0
     "Fizz"
 
 elsif int % 5 == 0
    "Buzz"
-
-
 
   if int % 5 || 3 != 0
  "nil"
