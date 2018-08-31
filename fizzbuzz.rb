809f76da4bc5c2469b5d39ffac32344dc1d238ef
Certil Remy <certil@noukod.com>
2018-08-31 16:29:50 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,19 +1,20 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
+elsif int % 5|| 3 == 0
+  "FizzBuzz"
 
-  if int % 3 == 0 # if the number int is divisible by 3
-    "Fizz" # Go fizz
-
+  if int % 3 == 0 
+    "Fizz" 
 
 elsif int % 5 == 0
    "Buzz"
 
-elsif int % 5||3 == 0
-  "FizzBuzz"
+
 
   if int % 5 || 3 != 0
  "nil"
+ 
 end
 end
 
